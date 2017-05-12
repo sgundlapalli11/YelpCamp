8474aa65e4ee10a036f0a0c4ddf3ec60675d4e57
Sai Gundlapalli <sgundlapalli@gatech.edu>
2017-05-12 22:39:10 +0000

    UI improvements

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get("/", function(req, res) {
 
 //show register form
 router.get("/register", function(req, res) {
-   res.render("register"); 
+   res.render("register", {page: 'register'}); 
 });
 
 router.post("/register", function(req, res) {
@@ -32,7 +32,7 @@ router.post("/register", function(req, res) {
 
 //show login form
 router.get("/login", function(req, res) {
-    res.render("login");
+    res.render("login", {page: 'login'});
 });
 // handling login form
 router.post("/login", passport.authenticate("local", 
